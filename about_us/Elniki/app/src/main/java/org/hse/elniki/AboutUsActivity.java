fa85127e30b352184a6dd6d5510c717c52b9a1ec
dariamm03 <osa.m@mail.ru>
2024-06-18 21:28:53 +0500

    about us

---
+++ about_us/Elniki/app/src/main/java/org/hse/elniki/AboutUsActivity.java
@@ -0,0 +1,18 @@
+package org.hse.elniki;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+    }
+
+}
